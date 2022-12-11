be8eb428eaa1a78be7797951a957ab4fb06390b2
Kooreis <kooreis.official@gmail.com>
2022-12-11 03:00:00 -0600

    This is the initial script that imports the qrcode library and defines a function to generate a QR code from input data and save it as an image file.

--- QR_Code_Generator.py
+++ QR_Code_Generator.py
@@ -1,5 +1,3 @@
-Sure, here is a simple Python script for a console application that generates QR codes:
-
 ```python
 import qrcode
 
@@ -15,16 +13,4 @@ def generate_qr(data, file_name):
 
     img = qr.make_image(fill_color="black", back_color="white")
     img.save(file_name)
-
-if __name__ == "__main__":
-    data = input("Enter the data for the QR code: ")
-    file_name = input("Enter the filename (with .png extension): ")
-    generate_qr(data, file_name)
-    print("QR Code generated successfully!")
-```
-
-Please note that you need to have the `qrcode` library installed. You can install it using pip:
-
-```bash
-pip install qrcode[pil]
 ```
