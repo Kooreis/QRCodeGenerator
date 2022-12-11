1537a121600911ecf80018e9f7083450a4583b19
Kooreis <kooreis.official@gmail.com>
2022-12-11 03:00:00 -0600

    Imports necessary libraries from ZXing for generating QR codes.

--- QR_Code_Generator.java
+++ QR_Code_Generator.java
@@ -1,37 +1,5 @@
-Here is a simple Java console application that generates a QR code:
-
-```java
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
-
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.util.Scanner;
-
-public class QRCodeGenerator {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter text to generate QR code:");
-        String text = scanner.nextLine();
-        System.out.println("Enter file path to save QR code:");
-        String filePath = scanner.nextLine();
-        generateQRCodeImage(text, 350, 350, filePath);
-    }
-
-    private static void generateQRCodeImage(String text, int width, int height, String filePath) {
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        try {
-            BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
-            Path path = FileSystems.getDefault().getPath(filePath);
-            MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
-        } catch (WriterException | IOException e) {
-            System.out.println("Could not generate QR Code: " + e.getMessage());
-        }
-    }
-}
-```
-
-Please note that this code uses the ZXing library for Java, which is not included in the standard Java library. You will need to import it into your project.
