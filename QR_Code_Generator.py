17589f647ecd46d502619e19e7adcc577ed90ae5
Kooreis <kooreis.official@gmail.com>
2022-12-12 03:00:00 -0600

    This update adds a main function that prompts the user to input the data for the QR code and the filename to save the image as.

--- QR_Code_Generator.py
+++ QR_Code_Generator.py
@@ -1,16 +1,6 @@
 ```python
-import qrcode
-
-def generate_qr(data, file_name):
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_H,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(data)
-    qr.make(fit=True)
-
-    img = qr.make_image(fill_color="black", back_color="white")
-    img.save(file_name)
+if __name__ == "__main__":
+    data = input("Enter the data for the QR code: ")
+    file_name = input("Enter the filename (with .png extension): ")
+    generate_qr(data, file_name)
 ```
