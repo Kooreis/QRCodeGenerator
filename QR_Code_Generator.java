b89e3151fb111480e0fe87a9072e002421550f35
Kooreis <kooreis.official@gmail.com>
2022-12-12 03:00:00 -0600

    Imports necessary Java libraries for handling file paths and user input.

--- QR_Code_Generator.java
+++ QR_Code_Generator.java
@@ -1,5 +1,3 @@
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.Scanner;
